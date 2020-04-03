ee61ed2b0d34f2c7c8c0bdf32901d425a2138a35
Viciivodka77 <Viciivodka77@outlook.com>
2020-04-03 17:10:29 +0800

    spring boot community day15   增加了不刷新页面登陆功能

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -11,7 +11,24 @@ function post() {
             "type":1
         }),
         success: function (response) {
-            console.log(response)
+            if(response.code == 200){
+                $("#comment_section").hide();
+            }else {
+                console.log(1);
+                if (response.code == 2003){
+                    var isAccepted = confirm(response.message);
+                    console.log(isAccepted);
+                    console.log(2);
+                    if (isAccepted == true){
+                        window.open("https://github.com/login/oauth/authorize?client_id=a745ccb34a925b0b435b&redirect_uri=http://localhost:8888/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable", true);
+                    }
+                }else {
+                    alert(response.message);
+                    console.log(3);
+                }
+            }
+            console.log(response);
         },
         dataType: "json"
     });
